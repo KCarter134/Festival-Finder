6a904d46047e7e669b9036dda5e24107b54fbf54
Kyle Carter <kcarter134@yahoo.com>
2023-01-29 21:04:25 -0500

    search bar and card section vh for styling purposes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,11 +3,11 @@
 // You can add client_id and optionally client_secret to the end of any valid url to authenticate your request.
 
 
-let secret = "9e575b187543b7dc56afdf8ec70a64a015793b5fe6d5bc85715056d9f32ec152";
 let clientID = "MzE2ODYzNzZ8MTY3NTAwODYxOS42MTkxODU"
 let endPoint = "https://api.seatgeek.com/2"
 let queryString = "https://api.seatgeek.com/2/events?client_id=" + clientID + "&taxonomies.name=concert"
-let queryStringAuth = "https://api.seatgeek.com/2/events?client_id=MYCLIENTID&client_secret=" + secret; //optional
+let searchValue = document.getElementsByClassName("text");
+let submitBtn = document.getElementsByClassName("submit")
 
 fetch(queryString)
     .then(response => {
@@ -17,3 +17,4 @@ fetch(queryString)
         console.log(data)
     });
 
+    sub
